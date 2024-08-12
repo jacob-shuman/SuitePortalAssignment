5dbde6b072e25c4f6f8d392439e679d9f6968e1f
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2024-08-12 00:06:09 -0400

    feat: deploy.yml github action

--- tests/test.ts
+++ tests/test.ts
@@ -1,13 +1,14 @@
 import { expect, test } from '@playwright/test';
 
-test('login successfully', async ({ page }) => {
-	await page.goto('/login');
-	await expect(page.locator('button[type="submit"]')).toBeVisible();
+test.describe('auth', () => {
+	test('login successfully', async ({ page }) => {
+		await page.goto('/login');
+		await expect(page.locator('button[type="submit"]')).toBeVisible();
 
-	await page.locator('input[name="email"]').fill('jacob.shuman7@gmail.com');
-	await page.locator('input[name="password"]').fill('testing');
-	await page.locator('button[type="submit"]').click();
+		await page.locator('input[name="email"]').fill('jacob.shuman7@gmail.com');
+		await page.locator('input[name="password"]').fill('testing');
+		await page.locator('button[type="submit"]').click();
 
-	await expect(page).toHaveURL(/.*\/admin$/);
-	await page.screenshot();
+		await expect(page).toHaveURL(/.*\/admin$/);
+	});
 });
